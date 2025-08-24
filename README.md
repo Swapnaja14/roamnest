844d50659c066af2c3927356d5ec1a003a60f273
Swapnaja Yadav <swapnajayadav2021@gmail.com>
2025-08-24 11:56:45 +0530

    Add listings and initialise database

---
+++ README.md
@@ -0,0 +1,4 @@
+Roamnest
+
+1. All models are in models directory
+2. All initialization of database is in init directory
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Listing = require('./models/listing.js');
 const port = 8080;
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/roamnest";
@@ -24,3 +25,17 @@ app.get("/", (req, res) => {
     res.send("Request received");
 });
 
+
+app.get("/testListing", async (req, res) => {
+    let sampleListing = new Listing({
+        title: "My New Villa",
+        description: "By the blue sea",
+        price: 1200,
+        location: "Kochi, Kerala",
+        country: "Kerala"
+    });
+
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("successfull testing");
+});
