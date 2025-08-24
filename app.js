c887466c2a3ccff2819621324029df39209e4326
Swapnaja Yadav <swapnajayadav2021@gmail.com>
2025-08-24 13:45:47 +0530

    Index Route and Show Route Created

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
+const path = require('path');
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true})); // To parse all data inside req
+
 const port = 8080;
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/roamnest";
@@ -25,7 +30,7 @@ app.get("/", (req, res) => {
     res.send("Request received");
 });
 
-
+// Testing route
 app.get("/testListing", async (req, res) => {
     let sampleListing = new Listing({
         title: "My New Villa",
@@ -38,4 +43,17 @@ app.get("/testListing", async (req, res) => {
     await sampleListing.save();
     console.log("sample was saved");
     res.send("successfull testing");
+});
+
+// Index route
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings});
+});
+
+// Show route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
 });
